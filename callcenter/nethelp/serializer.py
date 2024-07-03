d9ee4e71008a5b54c531b8967bca7c1994541ab4
Cris272500 <cristopherquincer@gmail.com>
2024-07-03 08:19:25 -0600

    ✅ se realizan modificaciones en serializers.py, urls.py y views.py se elimina las anteriores configuraciones para hacer las nuevas

--- callcenter/nethelp/serializer.py
+++ callcenter/nethelp/serializer.py
@@ -1,42 +1,53 @@
+# mis modelos
+from .models import Agente
+
+from django.contrib.auth.password_validation import validate_password
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
-from .models import Agente, Usuario, Area, Servicio, Subservicio, Ticket, ServicioDisponible, ServicioCliente
+from rest_framework.validators import UniqueValidator
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class AgenteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Agente
         fields = '__all__'
 
-class UsuarioSerializer(serializers.ModelSerializer):
+class MytokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['nombre'] = user.nombre
+        token['username'] = user.username
+        token['departamento'] = user.departamento
+        token['estado'] = user.estado
+        token['is_admin'] = user.is_admin
+        return token
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
     class Meta:
-        model = Usuario
+        model = Agente
         fields = '__all__'
 
-class AreaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Area
-        fields = '__all__'
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields don't match."})
 
-class ServicioSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Servicio
-        fields = '__all__'
+        return attrs
 
-class SubservicioSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Subservicio
-        fields = '__all__'
+    def create(self, validated_data):
+        agent = Agente.objects.create(
+            nombre=validated_data['nombre'],
+            username=validated_data['username'],
+            password=validated_data['password'],
+            departamento=validated_data['departamento'],
+            estado=validated_data['estado'],
+            is_admin=validated_data['is_admin']
+        )
+        agent.set_password(validated_data['password'])
+        agent.save()
 
-class TicketSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Ticket
-        fields = '__all__'
 
-class ServicioDisponibleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ServicioDisponible
-        fields = '__all__'
-
-class ServicioClienteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ServicioCliente
-        fields = '__all__'''
+        return agent
--- callcenter/nethelp/urls.py
+++ callcenter/nethelp/urls.py
@@ -1,25 +1,7 @@
-from django.urls import path, include
-# aqui mandamos todas las vistas de mis modelos serializadas 
-from .views import hola, AgenteView, UsuarioView, AreaView, ServicioView, SubservicioView, TicketView, ServicioDisponibleView, ServicioClienteView
-from .views import register, index, login_view
-from nethelp import views
-from rest_framework import routers
-from rest_framework.documentation import include_docs_urls
+from rest_framework_simplejwt.views import TokenRefreshView
 
-router = routers.DefaultRouter()
-router.register(r'agentes', AgenteView, 'agentes')
-router.register(r'usuarios', UsuarioView, 'usuarios')
-router.register(r'areas', AreaView, 'areas')
-router.register(r'servicios', ServicioView, 'servicios')
-router.register(r'subservicios', SubservicioView, 'subservicios')
-router.register(r'tickets', TicketView, 'tickets')
-router.register(r'servicios_disponibles', ServicioDisponibleView, 'servicios_disponibles')
-router.register(r'servicioclientes', ServicioClienteView, 'servicioclientes')
+from django.urls import path
 
-urlpatterns = [
-    path('', index, name='index'),
-    path('api/login/', login_view, name='login'),
-    path("api/v1/", include(router.urls)),
-    path('docs/', include_docs_urls(title="CallCenter API")),
-    path('register/', register, name='register'),
+url_patterns = [
+    path()
 ]
